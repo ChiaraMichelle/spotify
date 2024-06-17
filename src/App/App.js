2aebf72a6b66e54ed393fbe0bb5f44c8c438f982
Chiara Michelle <chiaramichelleb@gmail.com>
2024-06-17 23:33:14 +0200

    deleted curl part (mistake) from app.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
-  curl -X POST "https://accounts.spotify.com/api/token" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret"
-
   return (
     <div className="App">
       <header className="App-header">
